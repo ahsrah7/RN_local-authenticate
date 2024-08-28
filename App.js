f549acca7980e655e49998a6c12f2d908816e169
ahsrah7 <harsha.wardan7@gmail.com>
2024-08-28 16:32:14 +0530

    managed flow

--- App.js
+++ App.js
@@ -1,11 +1,32 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-
+import PrimaryButton from './components/PrimaryButton';
+import { useEffect, useState } from 'react';
+import * as LocalAuthentication from "expo-local-authentication";
 export default function App() {
+  const [loggedIn,setLoggedIn] = useState(false);
+
+async function authenticate(){
+  const result = await LocalAuthentication.authenticateAsync();
+  if(result.success){
+    setLoggedIn(true);
+  }
+  
+}
+ let content = (
+<PrimaryButton onPress={authenticate}>
+        Login
+      </PrimaryButton>
+ )
+
+ if(loggedIn){
+  content = <Text>{loggedIn? "Logged in successfully":"Uh oh! Access denied"}</Text>
+ }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      {content}
+      
     </View>
   );
 }
---
+++ constants/colors.js
@@ -0,0 +1,8 @@
+const Colors = {
+    primary500: '#1E201E',
+    primary600: '#3C3D37',
+    primary800: '#697565',
+    accent500: '#ECDFCC',
+}
+
+export default Colors;
